8e1483f05e351d0a6053ff203aed69902c0ee7b5
nandarionndr1 <leebet16@gmail.com>
2019-03-03 05:48:18 +0800

    set up crud

--- js/pickup.js
+++ js/pickup.js
@@ -1,5 +1,5 @@
 
-  var script_url = "https://script.google.com/macros/s/AKfycbxxe6OmsX9qMOzryXrkHw4QIzNceMeAD_1owGITlFbHQj1sjWIz/exec";
+  var script_url = "https://script.google.com/macros/s/AKfycbzigqbeJ2FQgi4iKgGUxuBO3Wbj6BSeCpbiDPyebQ/exec";
   
   // Make an AJAX call to Google Script
   function insert_value() {
@@ -80,11 +80,9 @@ var id1=	$("#id").val();
   
   // print the returned data
   function ctrlq(e) {
-  
-	
-	$("#re").html(e.result);
-	$("#re").css("visibility","visible");
-	read_value();
+    $("#re").html(e.result);
+    $("#re").css("visibility","visible");
+    //read_value();
 	
   }
   
@@ -97,20 +95,14 @@ $("#re").css("visibility","hidden");
    
    document.getElementById("loader").style.visibility = "visible";
  var url = script_url+"?action=read";
+ 
 
 $.getJSON(url, function (json) {
 
     // Set the variables from the results array
-   
-   
-  
-
-        // CREATE DYNAMIC TABLE.
-        var table = document.createElement("table");
-
-		
-
-        var header = table.createTHead();
+    // CREATE DYNAMIC TABLE.
+    var table = document.createElement("table");
+    var header = table.createTHead();
 		var row = header.insertRow(0);     
 		var cell1 = row.insertCell(0);
 		var cell2 = row.insertCell(1);
@@ -138,3 +130,54 @@ $.getJSON(url, function (json) {
     });
 	}
   
+  function reada_value() {
+    console.log("huwaaat");
+    var id1=	$("#id").val();
+    $("#re").css("visibility","hidden");
+    
+    var url = script_url+"?id="+id1+"&action=readtbl";
+  
+
+    var request = jQuery.ajax({
+      crossDomain: true,
+      url: url ,
+      method: "GET",
+      dataType: "jsonp"
+    }).done(function(json){
+
+     // Set the variables from the results array
+     // CREATE DYNAMIC TABLE.
+     var table = document.createElement("table");
+     var header = table.createTHead();
+     var row = header.insertRow(0);     
+     var cell1 = row.insertCell(0);
+     var cell2 = row.insertCell(1);
+   
+     cell1.innerHTML = "<b>ID</b>";
+     cell2.innerHTML = "<b>Name</b>";
+         
+         // ADD JSON DATA TO THE TABLE AS ROWS.
+         for (var i = 0; i < json.records.length; i++) {
+ 
+             tr = table.insertRow(-1);
+         var tabCell = tr.insertCell(-1);
+                 tabCell.innerHTML = json.records[i].ID;
+         tabCell = tr.insertCell(-1);
+         tabCell.innerHTML = json.records[i].NAME;
+             }
+       
+ 
+         // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+         var divContainer = document.getElementById("showData");
+         divContainer.innerHTML = "";
+         divContainer.appendChild(table);
+     document.getElementById("loader").style.visibility = "hidden";
+     $("#re").css("visibility","visible");
+    });
+    
+    document.getElementById("loader").style.visibility = "visible";
+
+ 
+  }
+      
+    
