e3288398bfe1663557d375d9657258a536da4630
nandarionndr1 <leebet16@gmail.com>
2019-03-03 07:14:21 +0800

    mvp doneee

--- js/pickup.js
+++ js/pickup.js
@@ -2,182 +2,41 @@
   var script_url = "https://script.google.com/macros/s/AKfycbzigqbeJ2FQgi4iKgGUxuBO3Wbj6BSeCpbiDPyebQ/exec";
   
   // Make an AJAX call to Google Script
-  function insert_value() {
-    
-	$("#re").css("visibility","hidden");
-	 document.getElementById("loader").style.visibility = "visible";
-	$('#mySpinner').addClass('spinner');
-
-var id1=	$("#id").val();
-	var name= $("#name").val();
-	
-	
-    var url = script_url+"?callback=ctrlq&name="+name+"&id="+id1+"&action=insert";
-  
-
-    var request = jQuery.ajax({
-      crossDomain: true,
-      url: url ,
-      method: "GET",
-      dataType: "jsonp"
-    });
-
-  }
-  
-  
- 
-  
-  
-  	function update_value(){
-	$("#re").css("visibility","hidden");
-     document.getElementById("loader").style.visibility = "visible";
-	
-	
-	var id1=	$("#id").val();
-	var name= $("#name").val();
-	
-	
-	
-    var url = script_url+"?callback=ctrlq&name="+name+"&id="+id1+"&action=update";
-  
-
-    var request = jQuery.ajax({
-      crossDomain: true,
-      url: url ,
-      method: "GET",
-      dataType: "jsonp"
-    });
-
-	
-  }
-
-  
- 
-  
-  
-  function delete_value(){
-	$("#re").css("visibility","hidden");
-     document.getElementById("loader").style.visibility = "visible";
-	$('#mySpinner').addClass('spinner');
-var id1=	$("#id").val();
-	var name= $("#name").val();
-	
-	
-    var url = script_url+"?callback=ctrlq&name="+name+"&id="+id1+"&action=delete";
-  
-
-    var request = jQuery.ajax({
-      crossDomain: true,
-      url: url ,
-      method: "GET",
-      dataType: "jsonp"
-    });
-
-  }
-
-
-  
-  
-  // print the returned data
-  function ctrlq(e) {
-    $("#re").html(e.result);
-    $("#re").css("visibility","visible");
-    //read_value();
-	
-  }
-  
-  
-
-  
-function read_value() {
-
-$("#re").css("visibility","hidden");
-   
-   document.getElementById("loader").style.visibility = "visible";
- var url = script_url+"?action=read";
- 
-
-$.getJSON(url, function (json) {
-
-    // Set the variables from the results array
-    // CREATE DYNAMIC TABLE.
-    var table = document.createElement("table");
-    var header = table.createTHead();
-		var row = header.insertRow(0);     
-		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
-	
-		cell1.innerHTML = "<b>ID</b>";
-		cell2.innerHTML = "<b>Name</b>";
-        
-        // ADD JSON DATA TO THE TABLE AS ROWS.
-        for (var i = 0; i < json.records.length; i++) {
-
-            tr = table.insertRow(-1);
-				var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = json.records[i].ID;
-				tabCell = tr.insertCell(-1);
-				tabCell.innerHTML = json.records[i].NAME;
-            }
-      
-
-        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-        var divContainer = document.getElementById("showData");
-        divContainer.innerHTML = "";
-        divContainer.appendChild(table);
-		document.getElementById("loader").style.visibility = "hidden";
-		$("#re").css("visibility","visible");
-    });
-	}
-  
-  function reada_value() {
+function reada_value() {
     console.log("huwaaat");
     var id1=	$("#id").val();
     $("#re").css("visibility","hidden");
-    
     var url = script_url+"?id="+id1+"&action=readtbl";
-  
-
     var request = jQuery.ajax({
       crossDomain: true,
       url: url ,
       method: "GET",
       dataType: "jsonp"
     }).done(function(json){
-
      // Set the variables from the results array
      // CREATE DYNAMIC TABLE.
-     var table = document.createElement("table");
-     var header = table.createTHead();
-     var row = header.insertRow(0);     
-     var cell1 = row.insertCell(0);
-     var cell2 = row.insertCell(1);
-   
-     cell1.innerHTML = "<b>ID</b>";
-     cell2.innerHTML = "<b>Name</b>";
-         
-         // ADD JSON DATA TO THE TABLE AS ROWS.
-         for (var i = 0; i < json.records.length; i++) {
- 
-             tr = table.insertRow(-1);
-         var tabCell = tr.insertCell(-1);
-                 tabCell.innerHTML = json.records[i].ID;
-         tabCell = tr.insertCell(-1);
-         tabCell.innerHTML = json.records[i].NAME;
-             }
-       
- 
-         // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-         var divContainer = document.getElementById("showData");
-         divContainer.innerHTML = "";
-         divContainer.appendChild(table);
+     $("#transactions").empty();
+     if (json.records.length != 0){
+        $("#re").html("No Transactions found :( ");
+     }
+      for (var i = 0; i < json.records.length; i++) {
+          date = new Date(json.records[i].Time_stamp);
+          timestr= date.getMonth() + "/"+ date.getDate()+"/"+date.getFullYear();
+          str="<tr>";
+          str+="<td>["+json.records[i].NAME+"]<br>"+timestr+"</td>";
+          str+="<td><b>"+json.records[i].CONSIGNEE+"</b><br>"+json.records[i].CONTACT+"</td>";
+          str+="<td>"+json.records[i].EXPECTED_AMOUNT+"<br>"+json.records[i].SERVICE_TYPE+"</td></tr>";
+          
+          $("#transactions").append(str);
+      }
      document.getElementById("loader").style.visibility = "hidden";
-     $("#re").css("visibility","visible");
-    });
-    
-    document.getElementById("loader").style.visibility = "visible";
 
- 
+     $("#query").animate({opacity:0.0});
+     $("#query").remove();
+     $(".data_container").animate({opacity:1.0});
+     $(".data_container").css('display','block');
+    });
+    document.getElementById("loader").style.visibility = "visible";
   }
       
     
